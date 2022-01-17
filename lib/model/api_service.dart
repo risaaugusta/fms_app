9eac17f999c55581d2ce3c63ee1179b2eede5335
risaaugusta <augustariris20@gmail.com>
2022-01-17 10:37:05 +0700

    push progress januari

--- lib/model/api_service.dart
+++ lib/model/api_service.dart
@@ -1,13 +1,30 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:fms_app/model/msequipment.dart';
 import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService{
+  Future<String?> getToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token');
+  }
+
   Future<MsEquipment> fetchOperator() async{
+    String? token = await ApiService().getToken();
+    // final response = await http.get(url, headers: {
+    //   'Content-Type': 'application/json',
+    //   'Accept': 'application/json',
+    //   'Authorization': 'Bearer $token',
+    // });
+
     final response = await http
-        .get(Uri.parse('https://10.10.0.223/backendapimaster/public/api/auth/Equipments'));
+        .get(Uri.parse('https://10.10.0.223/backendapimaster/public/api/auth/Equipments'), headers: {
+      HttpHeaders.authorizationHeader: "Bearer $token",
+    },);
 
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
